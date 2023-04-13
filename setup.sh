3a869ff125e1203eab55eefd8699feeb0805822f
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-04-13 12:37:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ git clone https://github.com/Cazeho/FileManager.git
 cd FileManager
 pip install -r rq.txt
 chmod +x run.sh
+chmod + app.py
 chmod +x reload.sh
 
 ln -s $PWD/run.sh /usr/local/bin/filemanager
